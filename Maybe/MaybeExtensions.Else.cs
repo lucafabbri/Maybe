1b8a7ef34992e2f76cf34f5124c5e78fed7c0988
Luca Fabbri <lucafabbri84@live.it>
2025-09-16 17:32:37 +0200

    feat: adds error return to Else

--- Maybe/MaybeExtensions.Else.cs
+++ Maybe/MaybeExtensions.Else.cs
@@ -21,6 +21,16 @@ public static partial class MaybeExtensions
             : fallbackValue;
     }
 
+    public static Maybe<TValue, TError> Else<TValue, TError>(
+        this in Maybe<TValue, TError> maybe,
+        TError forwardedError)
+        where TError : BaseError, new()
+    {
+        return maybe.IsSuccess
+            ? maybe.ValueOrThrow()
+            : forwardedError;
+    }
+
     /// <summary>
     /// Returns the success value or, if the Maybe is an error, the result of the provided fallback function.
     /// </summary>
@@ -32,6 +42,19 @@ public static partial class MaybeExtensions
         return maybe.IsSuccess
             ? maybe.ValueOrThrow()
             : fallbackFunc(maybe.ErrorOrThrow());
+    }
+
+    /// <summary>
+    /// Returns the success value or, if the Maybe is an error, the provided fallback error.
+    /// </summary>
+    public static Maybe<TValue, TError> Else<TValue, TError>(
+        this in Maybe<TValue, TError> maybe,
+        Func<TError, TError> fallbackErrorFunc)
+        where TError : BaseError, new()
+    {
+        return maybe.IsSuccess
+            ? maybe.ValueOrThrow()
+            : fallbackErrorFunc(maybe.ErrorOrThrow());
     }
 
     #endregion
@@ -50,6 +73,16 @@ public static partial class MaybeExtensions
         return maybe.Else(fallbackValue);
     }
 
+
+    public static async Task<Maybe<TValue, TError>> Else<TValue, TError>(
+        this Task<Maybe<TValue, TError>> maybeTask,
+        TError forwaredError)
+        where TError : BaseError, new()
+    {
+        var maybe = await maybeTask.ConfigureAwait(false);
+        return maybe.Else(forwaredError);
+    }
+
     /// <summary>
     /// Asynchronously returns the success value or, if the Maybe is an error, the result of the provided fallback function.
     /// </summary>
@@ -60,6 +93,16 @@ public static partial class MaybeExtensions
     {
         var maybe = await maybeTask.ConfigureAwait(false);
         return maybe.Else(fallbackFunc);
+    }
+
+
+    public static async Task<Maybe<TValue, TError>> Else<TValue, TError>(
+        this Task<Maybe<TValue, TError>> maybeTask,
+        Func<TError, TError> fallbackErrorFunc)
+        where TError : BaseError, new()
+    {
+        var maybe = await maybeTask.ConfigureAwait(false);
+        return maybe.Else(fallbackErrorFunc);
     }
 
     #endregion
@@ -79,6 +122,16 @@ public static partial class MaybeExtensions
             : await fallbackAsync(maybe.ErrorOrThrow());
     }
 
+    public static async Task<Maybe<TValue, TError>> ElseAsync<TValue, TError>(
+        this Maybe<TValue, TError> maybe,
+        Func<TError, Task<TError>> forwardedErrorAsync)
+        where TError : BaseError, new()
+    {
+        return maybe.IsSuccess
+            ? maybe
+            : await forwardedErrorAsync(maybe.ErrorOrThrow());
+    }
+
     /// <summary>
     /// Asynchronously returns the success value or, if the Maybe is an error, asynchronously executes the fallback function and returns its result.
     /// </summary>
@@ -89,6 +142,16 @@ public static partial class MaybeExtensions
     {
         var maybe = await maybeTask.ConfigureAwait(false);
         return await maybe.ElseAsync(fallbackAsync).ConfigureAwait(false);
+    }
+
+
+    public static async Task<Maybe<TValue, TError>> ElseAsync<TValue, TError>(
+        this Task<Maybe<TValue, TError>> maybeTask,
+        Func<TError, Task<TError>> fallbackErrorAsync)
+        where TError : BaseError, new()
+    {
+        var maybe = await maybeTask.ConfigureAwait(false);
+        return await maybe.ElseAsync(fallbackErrorAsync).ConfigureAwait(false);
     }
 
     #endregion
